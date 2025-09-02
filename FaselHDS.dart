66dd73fbfd15524cbf3523a43e67425ded717ace
umibekimgyo <umibekimgyo@gmail.com>
2025-09-02 10:37:06 +0300

    Create FaselHDS.dart

---
+++ FaselHDS.dart
@@ -0,0 +1,13 @@
+{
+  "name": "FaselHDS",
+  "pkg": "com.mangayomi.faselhds",
+  "version": 1,
+  "lang": "ar",
+  "nsfw": 0,
+  "sources": [
+    {
+      "name": "FaselHDS",
+      "baseUrl": "https://www.faselhds.life"
+    }
+  ]
+}
